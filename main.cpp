b97c1bdc28785fbecfb04263a7f317f8cc975126
lolonet <33363638+lolonet@users.noreply.github.com>
2017-11-21 17:52:50 +0800

    提供IO接口，将日志信息写入IO

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <cstdio>
 #include "log.h"
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	log_debug("for test %s %d\n", "lolonet", 100);
-	log_warn("for test %s %d\n", "lolonet", 100);
-	log_error("for test %s %d\n", "lolonet", 100);
-	log_boot("for test %s %d\n", "lolonet", 100);
+	log_init("./20171121/log/test.log", "./20171121/log/", 3);
+
+	log_debug("for test %s %d", "lolonet", 100);
+	log_warn("for test %s %d", "lolonet", 100);
+	log_error("for test %s %d", "lolonet", 100);
+	log_boot("for test %s %d", "lolonet", 100);
 	// log_debug("Hello lolonet");
 
 	set_log_level(2);
-	log_debug("for test 1 %s %d\n", "lolonet", 100);			// 被屏蔽了，打不出
-	log_error("for test 2 %s %d\n", "lolonet", 100);
-
+	log_debug("for test 1 %s %d", "lolonet", 100);			// 被屏蔽了，打不出
+	log_error("for test 2 %s %d", "lolonet", 100);
 
 	return 0;
 }
